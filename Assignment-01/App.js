2fdac427fed7c24c171cddef94e053e126f63f29
Amol Sasane <amolsasane1429@gmail.com>
2024-01-18 12:40:33 +0530

    feat: installed react

--- Assignment-01/App.js
+++ Assignment-01/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", {id: "heading"}, "Namaste React!");
           const root = ReactDOM.createRoot(document.getElementById ("root"));
           root.render(heading);
