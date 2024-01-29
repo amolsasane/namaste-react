628b0ebf350a8454cf5d978b00e59c9672d82ab7
Amol Sasane <amolsasane1429@gmail.com>
2024-01-29 16:51:28 +0530

    restaurent card

--- Lesson-04/code/App.js
+++ Lesson-04/code/App.js
@@ -2,16 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import myImage from "./Images/food-logo.png";
 
+function ResCard() {
+  return (
+    <div className="resCard">
+      <img
+        className="res-img"
+        alt="McDonald's"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/405645b3118d83e89db4c65361e43733"
+      />
+      <h3>McDonald's</h3>
+      <p>4.5</p>
+      <p>Burger, Fries, Coke</p>
+      <p>Delivery Time: 30m</p>
+    </div>
+  );
+}
+
 function Header() {
   return (
     <div className="header">
       <div className="img-container">
         <img className="img" src={myImage} alt="logo" />
       </div>
+
       <div className="heading-container">
         <h1 className="header-heading">FoodHub</h1>
       </div>
-      <nav className="nav">
+
+      <nav className="nav-container">
         <ul className="nav-list">
           <li className="nav-list-item">Home</li>
           <li className="nav-list-item">Search</li>
@@ -28,7 +46,9 @@ function Main() {
       <div className="btn-container">
         <button className="btn">Top Restaurents</button>
       </div>
-      <div className="res-card-container"></div>
+      <div className="res-card-container">
+        <ResCard />
+      </div>
     </div>
   );
 }
