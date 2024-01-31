a989f366b808c8ad5bd45d2f9348019279f3ec9e
Amol Sasane <amolsasane1429@gmail.com>
2024-01-31 15:54:01 +0530

    Top rated res filter

--- Lesson-05/code/components/Body.js
+++ Lesson-05/code/components/Body.js
@@ -1,14 +1,26 @@
 import restaurants from "../utils/mockData.js";
 import ResCard from "./ResCard.js";
+import { useState } from "react";
 
 function Body() {
+  const [restaurantsList, setRestaurantsList] = useState(restaurants);
   return (
     <div className="main">
       <div className="btn-container">
-        <button className="btn">Top Restaurents</button>
+        <button
+          className="btn"
+          onClick={() => {
+            const topRatedRes = restaurants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setRestaurantsList(topRatedRes);
+          }}
+        >
+          Top Rated Restaurents
+        </button>
       </div>
       <div className="res-card-container">
-        {restaurants.map((restaurant) => (
+        {restaurantsList.map((restaurant) => (
           <ResCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
