bb1008e89fb8afc368f9ce2a410219b905d4db9f
Amol Sasane <amolsasane1429@gmail.com>
2024-02-08 14:17:35 +0530

    shimmer effect added

---
+++ Lesson-06/code/components/Shimmer.js
@@ -0,0 +1,107 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+
+      <div className="shimmer-card">
+        <div className="shimmer-card-img"></div>
+        <div className="shimmer-card-details">
+          <div className="shimmer-card-heading"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+          <div className="shimmer-card-line-a"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
