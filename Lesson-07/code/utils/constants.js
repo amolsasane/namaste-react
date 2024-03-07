c1c29ce70738f267bd01acda357d14f18b862ff3
Amol Sasane <amolsasane1429@gmail.com>
2024-03-07 18:40:28 +0530

    swiggy menu api as constant

--- Lesson-07/code/utils/constants.js
+++ Lesson-07/code/utils/constants.js
@@ -1,4 +1,8 @@
-const RES_LOGO_LINK =
+export const RES_LOGO_LINK =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export default RES_LOGO_LINK;
+export const RES_MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5744018&lng=73.818406&restaurantId=";
+
+export const SWIGGY_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5744018&lng=73.818406&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
