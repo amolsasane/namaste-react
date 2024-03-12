236631faf3f6ac183d95d82c19bf8b3376e18d9a
Amol Sasane <amolsasane1429@gmail.com>
2024-03-12 15:00:47 +0530

    created class component

--- Lesson-08/code/components/About.js
+++ Lesson-08/code/components/About.js
@@ -1,5 +1,12 @@
+import User from "./UserClass";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="user-component-class">
+      <h1>About Us</h1>
+      <User />
+    </div>
+  );
 };
 
 export default About;
---
+++ Lesson-08/code/components/UserClass.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class User extends React.Component {
+  render() {
+    return (
+      <div className="user-coomponent-class">
+        <h1>I am Class Component</h1>
+        <h2></h2>
+      </div>
+    );
+  }
+}
+
+export default User;
