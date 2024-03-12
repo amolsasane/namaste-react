d9deed9311d18050dd4ac5a85a475486fc82549f
Amol Sasane <amolsasane1429@gmail.com>
2024-03-12 15:42:43 +0530

    created state variable in class component

--- Lesson-08/code/components/UserClass.js
+++ Lesson-08/code/components/UserClass.js
@@ -3,11 +3,19 @@ import React from "react";
 class User extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 1,
+      count2: 2,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-coomponent-class">
+        <h3>Count: {count}</h3>
+        <h3>Count 2: {count2} </h3>
         <h2>Name : {name}</h2>
         <h2>Location : {location}</h2>
       </div>
