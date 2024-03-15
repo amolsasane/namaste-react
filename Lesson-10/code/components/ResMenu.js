de8ab5d102deec91115c866c12350fd99e3b3847
Amol Sasane <amolsasane1429@gmail.com>
2024-03-15 18:14:27 +0530

    style: applied styles to res menu

--- Lesson-10/code/components/ResMenu.js
+++ Lesson-10/code/components/ResMenu.js
@@ -17,14 +17,14 @@ const ResMenu = () => {
 
   return (
     <div className="res-menu">
-      <h1>{name}</h1>
-      <h3>
+      <h1 className="font-bold text-[2rem] pt-4 text-center">{name}</h1>
+      <h3 className="pb-4  text-lg text-center">
         {cuisines.join(", ")} - {costForTwoMessage}
       </h3>
-      <h2>Menu</h2>
-      <ul>
+      <h2 className="text-center text-lg font-bold">Menu</h2>
+      <ul className="text-center m-auto w-[500px]">
         {itemCards.map((item) => (
-          <li key={item.card?.info?.id}>
+          <li className="text-justify" key={item.card?.info?.id}>
             {item.card?.info?.name} - Rs. {item.card?.info?.price / 100}
           </li>
         ))}
