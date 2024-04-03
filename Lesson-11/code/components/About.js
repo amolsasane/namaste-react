e2bf2e21408f41d7fff140726980eed5b19d49b4
Amol Sasane <amolsasane1429@gmail.com>
2024-04-03 22:00:54 +0530

    feat: context in class component

--- Lesson-11/code/components/About.js
+++ Lesson-11/code/components/About.js
@@ -1,10 +1,13 @@
 import User from "./UserClass";
+import UserContext from "../utils/UserContext";
 
 const About = () => {
   return (
     <div className="user-component-class">
       <h1>About Us</h1>
-
+      <UserContext.Consumer>
+        {({ loggedInUser }) => <h1 className="font-bold">{loggedInUser}</h1>}
+      </UserContext.Consumer>
       <User name={"Default User"} location={"Pune"} />
     </div>
   );
